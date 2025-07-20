bdfd3f7e74e4f34bd0a2eadfbbe7cc44d4f636d0
danielperesaraujo <daniperesaraujo17@gmail.com>
2025-07-20 13:26:23 -0300

    Atualização

---
+++ kanban/script.js
@@ -0,0 +1,14 @@
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  const data = ev.dataTransfer.getData("text");
+  const card = document.getElementById(data);
+  ev.target.closest('.column').appendChild(card);
+}
